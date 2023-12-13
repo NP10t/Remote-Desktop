ba64c756ade95adca232686078e91cd0cefcc984
NP10t <NP10t@gmail.com>
2023-12-13 23:01:46 +0700

    tuyet voi qua ae

--- srcNP/Client/MyClient.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "..\PNet\IncludeMe.h"
-
-class MyClient : public Client
-{
-	bool ProcessPacket(std::shared_ptr<Packet> packet) override;
-	void OnConnect(TCPConnection & newConnection) override;
-	void OnDisconnect(TCPConnection & lostConnection, std::string reason) override;
-};
