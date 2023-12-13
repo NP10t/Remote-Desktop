4e525f798d601596a3d445f5803a698cbc710980
TinyDragons101 <minhtanpham379@gmail.com>
2023-12-13 12:19:15 +0700

    Delete UI files

--- src/Client/App.cpp
+++ src/Client/App.cpp
@@ -4,12 +4,8 @@ wxIMPLEMENT_APP(App);
 
 bool App::OnInit()
 {
-    ConsoleFrame* consoleFrame = new ConsoleFrame("Console", wxPoint(200, 0), wxSize(500, 300));
-    consoleFrame->Show(true);
     MainFrameClient* mainFrameClient = new MainFrameClient("CLient ", wxPoint(50, 50), wxSize(500, 300));
     mainFrameClient->Center();
     mainFrameClient->Show();
-
-
     return true;
 }
